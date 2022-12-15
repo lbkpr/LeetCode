ef4486811a9e54fd08f38596813ae9343194c3a6
lbkpr <116188374+lbkpr@users.noreply.github.com>
2022-12-15 05:43:57 +0100

    Add files via upload

---
+++ String_to_Integer_leetCode.cpp
@@ -0,0 +1,74 @@
+﻿#include <iostream>
+#include <string>
+//#include <cstdint>
+
+using std::string;
+
+int myAtoi(string s) {
+    int i{}, temp{}, uni_lock{}, space_lock{1};
+    long long number{};
+    bool sgn = true;
+    while (i < s.size()) {
+        if (int(s.at(i)) == 32) {
+            if (space_lock) {
+                ++i;
+                ++space_lock;
+                continue;
+            }
+            else break;
+        }
+        
+        if (int(s.at(i)) == 43) {
+            if (!uni_lock) {
+                sgn = true;
+                ++uni_lock;
+                space_lock = 0;
+                ++i;
+                continue;
+            }
+            else break;
+        }
+        
+        if (int(s.at(i)) == 45) {
+            if (!uni_lock) {
+                sgn = false;
+                ++uni_lock;
+                space_lock = 0;
+                ++i;
+                continue;
+            }
+            else break;
+        }
+
+        if (int(s.at(i))<48 || int(s.at(i)) > 57) break;
+
+        while (i < s.size() && (47 < int(s.at(i))) && (int(s.at(i)) < 58)) {
+            if (number < 2147483649) {
+                if (i != temp && number) number *= 10;
+                number += abs(48 - int(s.at(i)));
+                temp = i;
+                ++i;
+                ++uni_lock;
+            }
+            else break;
+        }
+        if (uni_lock) break;
+        ++i;
+    }
+    if (sgn) {
+        if (number > 2147483647) return 2147483647;
+        else return number;
+    }
+    else {
+        if ((-1 * number) < (-2147483648)) return -2147483648;
+        return (number * (-1));
+    }
+}
+
+
+int main()
+{
+    string some{"  +341"};
+    std::cout << myAtoi(some)<<'\n';
+}
+
---
+++ Zigzag Conversion_leetCode.cpp
@@ -0,0 +1,32 @@
+﻿#include <iostream>
+#include <string>
+
+std::string convert(std::string s, uint16_t numRows) {
+    if (numRows == 1) return s;
+    else{
+        std::string zigzag_string{};
+        for (uint16_t i{}; i < numRows; i++) {
+                    if ((i == 0) || (i == numRows - 1)) {
+                        for (uint16_t j{}; (i+j) < s.size(); j += (numRows + abs(numRows - 2))) {
+                                zigzag_string += s.at(i + j);
+                        }
+                    }
+                    else {
+                        for (uint16_t j{}; abs(i+j-2*i) < s.size(); j += (numRows + abs(numRows - 2))){
+                            if (j) zigzag_string += s.at(i + j - 2*i);
+                            if((i+j)<s.size())
+                                zigzag_string += s.at(i + j);
+                        }
+                    }
+        }
+        return zigzag_string;
+    }
+}
+
+int main()
+{
+    std::string F{};
+    int nR{};
+    std::cin >> F>>nR;
+    std::cout << convert(F, nR);
+}
